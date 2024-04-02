967036f57dd7c31fe423d5545a4b37a23d9d444c
amabilechacon <60405498+amabilechacon@users.noreply.github.com>
2024-04-01 22:15:23 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -17,3 +17,10 @@ Digite um número:
 
 Dica: Lembre do comando de resto da divisão inteira!
 """
+número_inteiro = int(input("Digite um número inteiro:"))
+
+if número_inteiro % 2 == 0:
+    print("Par")
+    
+else:
+    print("Ímpar")
